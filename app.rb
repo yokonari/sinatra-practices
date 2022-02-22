39dcb14432cfd6f0cc8c2caff18af01dbd3d80fb
yokonari <kobayashikk06@gmail.com>
2022-02-22 11:50:23 +0900

    メモ一覧を取得するロジックを変更

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'json'
 
 class Data
   def self.read(id)
-    File.open("./json/memos#{id}.json", 'r') do |file|
+    File.open("./json/memos#{id.to_i}.json", 'r') do |file|
       JSON.parse(file.read, symbolize_names: true)
     end
   end
@@ -19,24 +19,29 @@ class Data
     }
     memo_json = data.to_json
 
-    File.open("./json/memos#{id}.json", 'w') do |file|
+    File.open("./json/memos#{id.to_i}.json", 'w') do |file|
       file.puts(memo_json.to_s)
     end
   end
 
   def self.delete(id)
-    File.delete("./json/memos#{id}.json")
+    File.delete("./json/memos#{id.to_i}.json")
   end
 end
 
-def assign_to_instance(**memo)
+def assign_to_instance(memo)
   @id = memo[:id]
   @title = memo[:title]
   @body = memo[:body]
 end
 
 get '/memos' do
-  @memos = Dir.glob('./json/*.json')
+  memos = Dir.glob('./json/*.json')
+  @memos_all = memos.map do |file|
+    File.open(file.to_s, 'r') do |memo|
+      JSON.parse(memo.read, symbolize_names: true)
+    end
+  end
   erb :index
 end
 
@@ -63,7 +68,7 @@ end
 
 patch '/memos/*' do |id|
   Data.write(id, params)
-  redirect "/memos/#{id}"
+  redirect "/memos/#{id.to_i}"
 end
 
 get '/memos/*' do |id|
